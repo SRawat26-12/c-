25a03bee62ecf45aab22dfbbfd2a74fa2b1bd66a
shivam <shivamrawat1053@gmail.com>
2024-05-10 14:54:04 +0530

    ch

--- strong.cpp
+++ strong.cpp
@@ -1,31 +1,37 @@
+
 #include<iostream>
 using namespace std;
-int main()
-{
-int n;
-int a=1,r;
-int sum=0;
-cout<<"enter number";
-cin>>n;
-int s=n;
-while(s!=0)
-{
-	
-	r=s%10;
-	for(int i=2;i<=r;i++)
-	{
-		a=a*i;
-	
-	
-	}
-		sum=sum+a;
-		s=s/10;
-
+void  swp(int n){
+   int a=n%10;
+   int c=a;
+   n=n/10;
+   int s=0;
+   while(n!=0){
+       int rem=n%10;
+       s=s*10+rem;
+       n=n/10;
+   }
+   int b=s%10;
+   s=s/10;
+   while(s!=0){
+    int rem=s%10;
+    a=a*10+rem;
+    s=s/10;
+   }
+   a=a*10+b;
+   if(c==0){
+    cout<<"0"<<a;
+   }
+   else{
+    cout<<a;
+   } 
+    
 }
-if(n==sum)
-cout<<"strong number";
-else
-cout<<"not strong";
-return 0;
-
+int main(){
+    swp(123);
+    cout<<"\n";
+    swp(2349586);
+    cout<<"\n";
+    swp(5670);
+    return 0;
 }
